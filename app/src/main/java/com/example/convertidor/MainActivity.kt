7243d2f34f07408482d2c13cba0fd180349f7c2d
Javier <a22311043@uthermosillo.edu.mx>
2023-10-20 08:51:33 -0700

    Se creo e hizo la logica de la conversion de grados

--- app/src/main/java/com/example/convertidor/MainActivity.kt
+++ app/src/main/java/com/example/convertidor/MainActivity.kt
@@ -2,10 +2,36 @@ package com.example.convertidor
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Spinner
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var selectedTemp = findViewById<Spinner>(R.id.temp_options)
+        var temp = findViewById<EditText>(R.id.temperature)
+        var button = findViewById<Button>(R.id.convert)
+        var result = findViewById<TextView>(R.id.result)
+
+        if (selectedTemp != null){
+            val adapter = ArrayAdapter(
+                 this,
+                 android.R.layout.simple_spinner_item,
+                 resources.getStringArray(R.array.convert_options)
+            )
+            selectedTemp.adapter = adapter
+        }
+        button.setOnClickListener{
+            temp.text
+        }
+    }
+    fun convert(){
+        //TODO Crear las conversiones
+        //Tarea: Traer las formulas de conversion
     }
 }
